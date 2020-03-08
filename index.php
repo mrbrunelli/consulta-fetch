ce2e11f58524ee2747ec70e887d9cab5afd9dae4
mrbrunelli <matheus.brunelli@gmail.com>
2020-03-07 21:32:44 -0300

    adicionando date_default_timezone_set

--- index.php
+++ index.php
@@ -1,10 +1,14 @@
+<?php
+date_default_timezone_set('America/Sao_Paulo');
+?>
+
 <!DOCTYPE html>
 <html lang="pt-br">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>FETCH</title>
+    <title>FETCH <?= date('d/m/Y H:i') ?></title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 </head>
 
